cdf400fc8c015e59f3d1223ef102995db8e97e4b
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-01 20:41:59 -0300

    feat: created user login and register

---
+++ src/infra/http/user/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import { UserController } from '../controllers/UserController';
+
+export const userRouter = Router();
+const userControllers = new UserController();
+
+userRouter.post('/register', userControllers.register);
+userRouter.post('/login', userControllers.login);

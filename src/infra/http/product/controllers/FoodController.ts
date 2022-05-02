710e23c36fc22335dee79e603709494b11ecf9e8
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-01 22:02:40 -0300

    feat: added function to find all foods

--- src/infra/http/product/controllers/FoodController.ts
+++ src/infra/http/product/controllers/FoodController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { v4 as uuid } from 'uuid';
 
 import { IFoodProps } from '../../../../domain/product/entities/IFoodEntity';
+import { Food } from '../../../../providers/database/product/FoodSchema';
 import { FoodPresentation } from '../presentation/FoodPresentation';
 
 export class FoodController {
@@ -23,5 +24,19 @@ export class FoodController {
     } catch (err) {
       return response.status(401).json(err);
     }
+  }
+  public async findAll(
+    request: Request,
+    response: Response
+  ): Promise<Response<IFoodProps>> {
+    const findAllFoods = await Food.find();
+
+    if (!findAllFoods) {
+      return response
+        .status(400)
+        .json('Unexpected error to search all products!');
+    }
+
+    return response.status(200).json(findAllFoods);
   }
 }

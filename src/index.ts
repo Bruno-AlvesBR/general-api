cc3c81beedabafdfa81832d160d46a3e92bc5040
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-07 14:54:51 -0300

    feat: added cors to project

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import { routes } from './infra/http/shared/routes';
 import './providers/database/connection';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);

6dc68d81d649f27f4e7b58bac63acfb0514c9107
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-07 14:55:20 -0300

    feat: up file with cors to heroku

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./infra/http/shared/routes");
 require("./providers/database/connection");
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(routes_1.routes);

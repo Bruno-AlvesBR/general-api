fc61edcc3c4525b76c37d55542b838030d9177c1
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-14 23:51:46 -0300

    fix: adjust two and more app in the same port

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);

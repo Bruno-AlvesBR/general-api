8d5e525b620a14917d7d53c92a86b355a54ddb25
Bruno-Alves-Startse <bruno.alves@startse.com>
2022-05-29 18:03:00 -0300

    feat: created routes

--- src/infra/http/product/routes/index.ts
+++ src/infra/http/product/routes/index.ts
@@ -5,7 +5,9 @@ import { FoodController } from '../controllers/FoodController';
 export const productRouter = Router();
 const foodController = new FoodController();
 
-productRouter.post('/create', foodController.create);
 productRouter.get('/foods', foodController.findAll);
 productRouter.get('/:id', foodController.findById);
+productRouter.get('/count', foodController.count);
+productRouter.post('/create', foodController.create);
 productRouter.put('/:id', foodController.update);
+productRouter.delete('/:id', foodController.delete);

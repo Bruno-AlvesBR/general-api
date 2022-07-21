cb2df9a8c0e202472a144a95bf138bcb3462bcf2
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-07-20 21:03:30 -0300

    feat: added product data provider on register singleton

--- src/database/providers/injections.ts
+++ src/database/providers/injections.ts
@@ -1,8 +1,15 @@
 import IPodcastData from '@domain/podcast/data';
+import IProductData from '@domain/product/data';
 import { container } from 'tsyringe';
 import PodcastDataProvider from './podcasts';
+import ProductDataProvider from './products';
 
 container.registerSingleton<IPodcastData>(
   'PodcastDataProvider',
   PodcastDataProvider
+);
+
+container.registerSingleton<IProductData>(
+  'ProductDataProvider',
+  ProductDataProvider
 );

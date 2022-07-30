2ff2dc3f12d220c7022d60d86caae74151bc67d6
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-07-30 16:22:35 -0300

    feat: formated video prosp

--- src/infra/http/videos/presentation/VideoCreatePresentation.ts
+++ src/infra/http/videos/presentation/VideoCreatePresentation.ts
@@ -1,4 +1,4 @@
-import { IVideoProps } from '@domain/videos/entities';
+import { IVideoBody, IVideoProps } from '@domain/videos/entities';
 import VideoCreateUseCase from '../../../../domain/videos/useCases/VideoCreateUseCase';
 import IPresentation from 'core/Presentation';
 import { container, injectable } from 'tsyringe';
@@ -6,14 +6,22 @@ import { v4 as uuid } from 'uuid';
 
 @injectable()
 export default class VideoCreatePresentation
-  implements IPresentation<IVideoProps, IVideoProps>
+  implements IPresentation<IVideoBody, IVideoProps>
 {
-  public async handle(props: IVideoProps) {
+  public async handle(props: IVideoBody) {
     const videoCreateUseCase = container.resolve(VideoCreateUseCase);
 
     const videoObject = {
       id: uuid(),
-      ...props,
+      title: props?.title,
+      description: props?.description,
+      duration: props?.duration,
+      rating: props?.rating,
+      file: {
+        fileUrl: props?.fileUrl,
+        fileType: props?.fileType,
+        fileImage: props?.fileImage,
+      },
     };
 
     const createVideo = await videoCreateUseCase.execute(videoObject);

90641e13c64478f8bac6517f859f3cc3c34c3447
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-09-04 19:02:26 -0300

    feat: up files to heroku

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,12 @@ require("reflect-metadata");
 require("./database/connection");
 require("./database/providers/injections");
 const routes_1 = require("./infra/http/shared/routes");
+const middlewareAuthToken = require('./infra/http/shared/middlewares/Auth');
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use(middlewareAuthToken);
 app.use(routes_1.routes);
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
---
+++ dist/infra/http/shared/middlewares/Auth.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+module.exports = (request, response, next) => {
+    const authTokenHeader = request.headers.authorization;
+    if (!authTokenHeader)
+        return response.status(401).send({ error: 'Cannot find token!' });
+    const splitedHeaderToken = authTokenHeader.split(' ');
+    if (splitedHeaderToken.length !== 2)
+        return response.status(401).send({ error: 'Token error!' });
+    const [scheme, token] = splitedHeaderToken;
+    if (!/^Bearer$/i.test(scheme))
+        return response.status(401).send({ error: 'Token malformed!' });
+    jsonwebtoken_1.default.verify(token, `${process.env.SECRET_JWT}`, (err, decoded) => {
+        if (err)
+            return response.status(401).send({ error: 'Invalid token!' });
+        request.body.id = decoded === null || decoded === void 0 ? void 0 : decoded.id;
+        return next();
+    });
+};

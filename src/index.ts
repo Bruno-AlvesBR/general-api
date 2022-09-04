d9813dd1637cedba4615113f1240aadf0e397c7f
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-09-04 19:01:50 -0300

    feat: added token authorization to api

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import cors from 'cors';
 import 'reflect-metadata';
 import './database/connection';
 import './database/providers/injections';
+
 import { routes } from './infra/http/shared/routes';
+const middlewareAuthToken = require('./infra/http/shared/middlewares/Auth');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(middlewareAuthToken);
 app.use(routes);
 
 const port = process.env.PORT || 3333;
---
+++ src/infra/http/shared/middlewares/Auth.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+import { IUserProps } from '@domain/user/entities/IUserEntity';
+
+module.exports = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const authTokenHeader = request.headers.authorization;
+
+  if (!authTokenHeader)
+    return response.status(401).send({ error: 'Cannot find token!' });
+
+  const splitedHeaderToken = authTokenHeader.split(' ');
+
+  if (splitedHeaderToken.length !== 2)
+    return response.status(401).send({ error: 'Token error!' });
+
+  const [scheme, token] = splitedHeaderToken;
+
+  if (!/^Bearer$/i.test(scheme))
+    return response.status(401).send({ error: 'Token malformed!' });
+
+  jwt.verify(
+    token,
+    `${process.env.SECRET_JWT}`,
+    (err, decoded: IUserProps | any) => {
+      if (err)
+        return response.status(401).send({ error: 'Invalid token!' });
+
+      request.body.id = decoded?.id;
+      return next();
+    }
+  );
+};

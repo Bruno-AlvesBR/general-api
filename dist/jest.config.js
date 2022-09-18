975e40fbc59eb33a4d7be061b68384c784f1f419
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-09-18 15:16:12 -0300

    feat: up files to heroku

---
+++ dist/jest.config.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports = {
+    bail: true,
+    verbose: true,
+    clearMocks: true,
+    collectCoverage: false,
+    collectCoverageFrom: ['<rootDir>/src/domain/**/useCases/*.ts'],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text-summary', 'lcov'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.spec.ts'],
+    setupFiles: ['<rootDir>/test.ts'],
+};
